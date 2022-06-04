c7a276d8db770a4c78e7c5b9cca17675bdeecbe5
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-06-04 19:38:32 +0500

    Lecture 5
    Advance Python - OOP implementation.

--- python_help.py
+++ python_help.py
@@ -1,12 +1,1 @@
-name = 'This is a global name'
-
-def greet():
-    # Enclosing function
-    # name = 'Hammad Ali'
-    global name
-    print('this is before global valiable overwrite {}'.format(name))
-    name = "Hammad Ali"
-    print('New variable value {}'.format(name))
-
-greet()
-print(name)
+print('Hello
