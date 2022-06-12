48b6f7531f3c8ac073f925b21bd978885a3cd6b6
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-06-12 08:36:59 +0500

    Lecture 7
    Templates and Static files.

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello World')
+    # return HttpResponse('Hello World')
+    # my_dict = {'insert_me': "I am pleased to be here"}
+    # return render(request, 'index.html', context=my_dict)
+    return render(request, 'first_app/index.html')
