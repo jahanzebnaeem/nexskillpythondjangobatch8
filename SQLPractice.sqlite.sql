4e0b2eec9e0c277ca730746b1c504945001cb4fd
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-06-13 15:34:17 +0500

    Lecture 8
    SQL practice.

---
+++ SQLPractice.sqlite.sql
@@ -0,0 +1,21 @@
+-- TABLE
+CREATE TABLE City (
+    Cityid int NOT NULL,
+    Name varchar(255) NOT NULL,
+  	Countryid Int NOT NULL,
+    PRIMARY KEY (Cityid)
+  	FOREIGN KEY (Countryid) REFERENCES Country(Countryid)
+);
+CREATE TABLE County (
+    Countryid int NOT NULL,
+    Name varchar(255) NOT NULL,
+    PRIMARY KEY (Countryid)
+);
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
