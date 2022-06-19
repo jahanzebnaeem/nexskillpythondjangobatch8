9d73736d406b374bf75c9b7fe9a335c296217256
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-06-19 05:32:43 +0500

    Lecture 9
    Creating models and using MTV.

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic, Webpage, AccessRecord
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello World')
     # my_dict = {'insert_me': "I am pleased to be here"}
     # return render(request, 'index.html', context=my_dict)
-    return render(request, 'first_app/index.html')
+    # return render(request, 'first_app/index.html')
+    topic_list = Topic.objects.all()
+    my_topic_dict = {'topic_title': topic_list}
+    return render(request, 'first_app/index.html', context=my_topic_dict)
