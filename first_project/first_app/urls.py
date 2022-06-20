30eaa0f2113120fc5732c92172bfe24db38b35e6
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-06-20 08:11:02 +0500

    Lecture 10
    Django forms and validations.

--- first_project/first_app/urls.py
+++ first_project/first_app/urls.py
@@ -3,4 +3,6 @@ from first_app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('formindex/', views.form_index, name='form_index'),
+    path('formpage/', views.form_name_view, name='form_page'),
 ]
--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from first_app.models import Topic, Webpage, AccessRecord
+from . import forms
 
 # Create your views here.
 def index(request):
@@ -11,3 +12,21 @@ def index(request):
     topic_list = Topic.objects.all()
     my_topic_dict = {'topic_title': topic_list}
     return render(request, 'first_app/index.html', context=my_topic_dict)
+
+def form_index(request):
+    return render(request, 'first_app/form_index.html')
+
+def form_name_view(request):
+    form = forms.FormName()
+
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+
+        if form.is_valid():
+            # DO SOMETHING CODE
+            print("Validation successful")
+            print("Name: "+form.cleaned_data['name'])
+            print("Email: "+form.cleaned_data['email'])
+            print("Text: "+form.cleaned_data['text'])
+
+    return render(request, 'first_app/form_page.html', {'form':form})
